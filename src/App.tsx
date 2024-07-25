484caedbc2628d15916e2e967cc6c2f5f9ac0bce
Weiyi Lee <weiyilee17@gmail.com>
2024-07-25 22:01:00 +0800

    Have atom and useAtom work

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { atom, useAtom } from './jotai';
+
+const salaryAtom = atom<number>(100_000);
+
+function SalaryDisplay() {
+  const [salary] = useAtom<number>(salaryAtom);
+  return <div>{salary}</div>;
+}
+
 function App() {
-  return <>hi</>;
+  const [salary, setSalary] = useAtom<number>(salaryAtom);
+
+  return (
+    <>
+      <div>
+        <label htmlFor='salary'>Salary </label>
+        <input
+          type='number'
+          id='salary'
+          value={salary}
+          onChange={(e) => setSalary(+e.target.value)}
+        />
+      </div>
+      <div>{salary}</div>
+      <SalaryDisplay />
+    </>
+  );
 }
 
 export default App;
---
+++ src/jotai.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+type Atom<AtomType> = {
+  get: () => AtomType;
+  set: (newValue: AtomType) => void;
+  // Returns the unsubscribe function
+  subscribe: (callback: (newValue: AtomType) => void) => () => void;
+};
+
+export function atom<AtomType>(initialValue: AtomType): Atom<AtomType> {
+  let value = initialValue;
+
+  const subscribers = new Set<(newValue: AtomType) => void>();
+
+  return {
+    get: () => value,
+    set: (newValue) => {
+      value = newValue;
+      subscribers.forEach((callback) => callback(value));
+    },
+    subscribe: (callback) => {
+      subscribers.add(callback);
+
+      return () => {
+        subscribers.delete(callback);
+      };
+    },
+  };
+}
+export function useAtom<AtomType>(atom: Atom<AtomType>) {
+  const [value, setValue] = useState(atom.get());
+
+  useEffect(() => {
+    // Use setState to trigger rerender
+    const unsubscribe = atom.subscribe(setValue);
+
+    return () => {
+      unsubscribe();
+    };
+  }, [atom]);
+
+  return [value, atom.set];
+}
